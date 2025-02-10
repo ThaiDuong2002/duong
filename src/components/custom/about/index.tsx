5d6d7806a758eac8a53986a542f5fe8178618375
Thái Dương <105373894+ThaiDuong2002@users.noreply.github.com>
2025-02-10 15:51:45 +0700

    52 - Responsive

--- src/components/custom/about/index.tsx
+++ src/components/custom/about/index.tsx
@@ -28,13 +28,13 @@ const About = ({ id }: { id?: string }) => {
             />
           </div>
           <div className="flex flex-col justify-center items-start max-w-lg">
-            <h1 className="mt-4 font-bold text-gray-900 dark:text-white text-4xl">
+            <h1 className="mt-4 font-bold text-gray-900 dark:text-white max-sm:text-xl max-md:text-3xl text-4xl">
               {aboutT(about.title)}
             </h1>
-            <p className="mt-4 text-gray-700 dark:text-gray-300 text-lg">
+            <p className="mt-4 text-gray-700 max-md:text-md dark:text-gray-300 max-sm:text-sm text-lg">
               {aboutT(about.paragraph1)}
             </p>
-            <p className="mt-4 text-gray-700 dark:text-gray-300 text-lg">
+            <p className="mt-4 text-gray-700 max-md:text-md dark:text-gray-300 max-sm:text-sm text-lg">
               {aboutT(about.paragraph2)}
             </p>
           </div>
@@ -46,20 +46,20 @@ const About = ({ id }: { id?: string }) => {
                 size={40}
                 className="text-blue-600 dark:text-blue-400"
               />
-              <h1 className="font-bold text-gray-900 dark:text-white text-4xl">
+              <h1 className="font-bold text-gray-900 dark:text-white max-sm:text-xl max-md:text-3xl text-4xl">
                 {educationT(education.title)}
               </h1>
             </div>
-            <p className="mt-4 font-medium text-gray-700 dark:text-gray-300 text-lg leading-none">
+            <p className="mt-4 max-md:mt-1 font-medium text-gray-700 max-md:text-md dark:text-gray-300 max-sm:text-sm text-lg leading-none">
               {educationT(education.university)}
             </p>
-            <p className="mt-4 font-medium text-gray-700 dark:text-gray-300 text-lg leading-none">
+            <p className="mt-4 max-md:mt-2 font-medium text-gray-700 max-md:text-md dark:text-gray-300 max-sm:text-sm text-lg leading-none">
               {educationT(education.degree)}
             </p>
-            <p className="mt-4 font-medium text-gray-700 dark:text-gray-300 text-lg leading-none">
+            <p className="mt-4 max-md:mt-2 font-medium text-gray-700 max-md:text-md dark:text-gray-300 max-sm:text-sm text-lg leading-none">
               {educationT(education.gpa)}
             </p>
-            <p className="mt-4 font-medium text-gray-700 dark:text-gray-300 text-lg leading-none">
+            <p className="mt-4 max-md:mt-2 font-medium text-gray-700 max-md:text-md dark:text-gray-300 max-sm:text-sm text-lg leading-none">
               {educationT(education.award1)}
             </p>
           </div>
@@ -69,14 +69,14 @@ const About = ({ id }: { id?: string }) => {
                 size={40}
                 className="text-yellow-600 dark:text-yellow-400"
               />
-              <h1 className="font-bold text-gray-900 dark:text-white text-4xl">
+              <h1 className="font-bold text-gray-900 dark:text-white max-sm:text-xl max-md:text-3xl text-4xl">
                 {educationT(education.certificates)}
               </h1>
             </div>
-            <p className="mt-4 font-medium text-gray-700 dark:text-gray-300 text-lg leading-none">
+            <p className="mt-4 max-md:mt-2 font-medium text-gray-700 max-md:text-md dark:text-gray-300 max-sm:text-sm text-lg leading-none">
               {educationT(education.certificate1)}
             </p>
-            <p className="mt-4 font-medium text-gray-700 dark:text-gray-300 text-lg leading-none">
+            <p className="mt-4 max-md:mt-2 font-medium text-gray-700 max-md:text-md dark:text-gray-300 max-sm:text-sm text-lg leading-none">
               {educationT(education.certificate2)}
             </p>
           </div>
