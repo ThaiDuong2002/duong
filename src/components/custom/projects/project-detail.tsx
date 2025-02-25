3458898932ac7f0e657b8c950ce2668b61812a3e
Thái Dương <105373894+ThaiDuong2002@users.noreply.github.com>
2025-02-25 11:06:43 +0700

    98 - Chat App #9: Overview

--- src/components/custom/projects/project-detail.tsx
+++ src/components/custom/projects/project-detail.tsx
@@ -1,43 +1,8 @@
 "use client";
 
 import ImageCarousel from "@/components/custom/projects/image-carousel";
+import { ProjectDetailProps } from "@/interface/components/project-detail";
 import { useTranslations } from "next-intl";
-
-interface ProjectDetailProps {
-  id: string;
-  imageListOption?: "multiple" | "single";
-  detailInfo: {
-    imageList: string[] | null;
-    overview: {
-      id: string;
-      name: string;
-      applications: {
-        application: string;
-        overviewList: string[];
-        imageList: string[] | null;
-      }[];
-    };
-    technologies: {
-      id: string;
-      name: string;
-      techs: {
-        application: string;
-        techList: { name: string; description: string }[];
-        techImages: string[] | null;
-      }[];
-    };
-    architectures: {
-      id: string;
-      name: string;
-      architectureList:
-        | {
-            image: string;
-            architectureList: string[];
-          }[]
-        | null;
-    };
-  };
-}
 
 const ProjectDetail = ({
   id,
@@ -47,19 +12,35 @@ const ProjectDetail = ({
   const t = useTranslations(id);
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col mt-10">
       {imageListOption === "single" && detailInfo.imageList && (
         <ImageCarousel list={detailInfo.imageList} className="my-10" />
       )}
-      <div className="flex flex-col gap-4 mx-10 max-sm:mx-0 lg:mx-32">
-        <div className="border-b-2 border-black font-bold text-gray-900 dark:text-white max-sm:text-sm max-md:text-xl text-2xl">
+      <div className="flex flex-col gap-4">
+        <div className="mx-10 max-sm:mx-0 lg:mx-32 border-b-2 border-black font-bold text-gray-900 dark:text-white max-sm:text-lg max-md:text-2xl text-3xl">
           {detailInfo.overview.name}
         </div>
-        <div className="border-b-2 border-black font-bold text-gray-900 dark:text-white max-sm:text-sm max-md:text-xl text-2xl">
+        {detailInfo.overview.applications.map((application, index) => (
+          <div className="flex flex-col" key={index}>
+            <div>
+              <ul className="mx-10 max-sm:mx-0 lg:mx-32 text-gray-600 max-sm:text-sm max-md:text-lg text-xl list-disc list-inside">
+                {application.overviewList.map((overview, index) => (
+                  <li key={index}>
+                    {t(`${detailInfo.overview.id}.${overview}`)}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {imageListOption === "multiple" && application.imageList && (
+              <ImageCarousel list={application.imageList} className="my-10" />
+            )}
+          </div>
+        ))}
+        <div className="mx-10 max-sm:mx-0 lg:mx-32 border-b-2 border-black font-bold text-gray-900 dark:text-white max-sm:text-lg max-md:text-2xl text-3xl">
           {detailInfo.technologies.name}
         </div>
         {detailInfo.architectures.architectureList && (
-          <div className="border-b-2 border-black font-bold text-gray-900 dark:text-white max-sm:text-sm max-md:text-xl text-2xl">
+          <div className="mx-10 max-sm:mx-0 lg:mx-32 border-b-2 border-black font-bold text-gray-900 dark:text-white max-sm:text-lg max-md:text-2xl text-3xl">
             {detailInfo.architectures.name}
           </div>
         )}
---
+++ src/interface/components/project-detail.ts
@@ -0,0 +1,37 @@
+interface ProjectDetailProps {
+  id: string;
+  imageListOption?: "multiple" | "single";
+  detailInfo: {
+    imageList: string[] | null;
+    overview: {
+      id: string;
+      name: string;
+      applications: {
+        application: string;
+        overviewList: string[];
+        imageList: string[] | null;
+      }[];
+    };
+    technologies: {
+      id: string;
+      name: string;
+      techs: {
+        application: string;
+        techList: { name: string; description: string }[];
+        techImages: string[] | null;
+      }[];
+    };
+    architectures: {
+      id: string;
+      name: string;
+      architectureList:
+        | {
+            image: string;
+            architectureList: string[];
+          }[]
+        | null;
+    };
+  };
+}
+
+export type { ProjectDetailProps };
