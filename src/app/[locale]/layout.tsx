b1a4aed4424e04fa99a678ad436e363dfe579682
Thái Dương <105373894+ThaiDuong2002@users.noreply.github.com>
2025-02-27 10:35:38 +0700

    118 - Add Tabs UI

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,7 +6,7 @@ import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { Geist, Geist_Mono } from "next/font/google";
 import { notFound } from "next/navigation";
-import "./globals.css";
+import "../globals.css";
 import { ReactNode } from "react";
 
 const geistSans = Geist({
