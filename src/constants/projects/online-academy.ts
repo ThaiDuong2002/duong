8c3eaddc9232333575bfe22dc8608d065e8012b1
Thái Dương <105373894+ThaiDuong2002@users.noreply.github.com>
2025-02-28 10:13:48 +0700

    125 - Online Academy internationalization

--- src/constants/projects/online-academy.ts
+++ src/constants/projects/online-academy.ts
@@ -40,7 +40,13 @@ const OnlineAcademyConstants: ProjectInterface = {
         {
           id: "frontend",
           application: "Frontend",
-          overviewList: ["overview-01", "overview-02", "overview-03"],
+          overviewList: [
+            "overview-01",
+            "overview-02",
+            "overview-03",
+            "overview-04",
+            "overview-05",
+          ],
           imageList: null,
         },
       ],
